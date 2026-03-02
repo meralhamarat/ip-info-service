0c24382fd4525914f4fc4218ee4fb794bc68274d
Ömer Faruk KESMEZ <31587367+ofke-yazilim@users.noreply.github.com>
2026-03-02 11:33:29 +0300

    Örnek swagger kullanımı tanımlandı.

--- src/swagger.php
+++ src/swagger.php
@@ -1,1 +1,41 @@
-php
+<?php
+require 'vendor/autoload.php';
+
+use OpenApi\Generator;
+use OpenApi\Attributes as OA;
+
+#[OA\Info(title: "My Simple API", version: "1.0.0")]
+class OpenApiConfig {}
+
+// Basit bir endpoint tanımı
+#[OA\Get(
+    path: "/hello",
+    operationId: "helloWorld",
+    responses: [
+        new OA\Response(response: 200, description: "Returns Hello World")
+    ]
+)]
+function helloWorld() {
+    header('Content-Type: application/json');
+    echo json_encode(["message" => "Hello World"]);
+}
+
+// Router mantığı
+$requestUri = $_SERVER['REQUEST_URI'];
+
+if ($requestUri === '/hello') {
+    helloWorld();
+    exit;
+}
+
+if ($requestUri === '/docs') {
+    // Swagger dokümantasyonu üret
+    $openapi = Generator::scan([__DIR__]);
+    header('Content-Type: application/json');
+    echo $openapi->toJson();
+    exit;
+}
+
+// Varsayılan cevap
+header('Content-Type: text/plain');
+echo "Available endpoints: /hello, /docs";
